84e5c3861f4a8d0f8e262703ec18210c1a50735f
Avni <avnishah2009@gmail.com>
2018-07-24 23:03:49 -0700

    Update kth_smallest_level_3.py

--- kth_smallest_level_3.py
+++ kth_smallest_level_3.py
@@ -1,14 +1,18 @@
 import heapq
 
+class Solution:
+    # @param A : tuple of integers
+    # @param B : integer
+    # @return an integer
 
-def kthsmallest(A, B):
-    heap = []
-    for number in A:
-        heapq.heappush(heap, number)
-
-    for i in range(B):
-        number = heapq.heappop(heap)
-
-    return number
-
-print kthsmallest([2,1,3,4,], 3)
+    def kthsmallest(self, A, B):
+        heap = []
+        for number in A:
+            heapq.heappush(heap, number)
+    
+        for i in range(B):
+            number = heapq.heappop(heap)
+    
+        return number
+    
+    #rint kthsmallest([2,1,3,4,], 3)
