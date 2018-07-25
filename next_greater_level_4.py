7dd8e90dcd5e94ec5c35299b217d7d60c02ea68e
Avni <avnishah2009@gmail.com>
2018-07-24 23:06:02 -0700

    next_greater_level_4

--- next_greater_level_4.py
+++ next_greater_level_4.py
@@ -1,19 +1,22 @@
-def nextGreater(A):
-    a_len = len(A)
-    output = []
-    for ind, number in enumerate(A):
-        j = ind+1
-        found_greater_val = False
-        while j < a_len:
-            if A[j] > number:
-                output.append(A[j])
-                found_greater_val = True
-                break
-            j += 1
-        if not found_greater_val:
-            output.append(-1)
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    
+    def nextGreater(self, A):
+        a_len = len(A)
+        output = []
+        for ind, number in enumerate(A):
+            j = ind+1
+            found_greater_val = False
+            while j < a_len:
+                if A[j] > number:
+                    output.append(A[j])
+                    found_greater_val = True
+                    break
+                j += 1
+            if not found_greater_val:
+                output.append(-1)
+        
+        return output
 
-    #output.append(-1)
-    return output
-
-print nextGreater([3, 2, 1])
+#print nextGreater([3, 2, 1])
